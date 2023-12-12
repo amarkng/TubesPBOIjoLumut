080b31c7e81baa257b6b3876a90206f703db6985
amarkng <ramadhansachra123@gmail.com>
2023-12-12 20:39:28 +0700

    3kelas

--- src/item.java
+++ src/item.java
@@ -1,3 +1,6 @@
+
+import java.util.Date;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,58 @@
  * @author ImNotplying
  */
 public class item {
+    private int uniqueID;
+    private int produkID;
+    private String itemName;
+    private int lokasi;
+    private int quantity;
     
+    public item(int uniqueID, int produkID, String itemName, int lokasi, int quantity){
+        this.uniqueID=uniqueID;
+        this.produkID=produkID;
+        this.itemName=itemName;
+        this.lokasi=lokasi;
+        this.quantity=quantity;
+        
+    }
+
+    public int getUniqueID() {
+        return uniqueID;
+    }
+
+    public void setUniqueID(int uniqueID) {
+        this.uniqueID = uniqueID;
+    }
+
+    public int getProdukID() {
+        return produkID;
+    }
+
+    public void setProdukID(int produkID) {
+        this.produkID = produkID;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getLokasi() {
+        return lokasi;
+    }
+
+    public void setLokasi(int lokasi) {
+        this.lokasi = lokasi;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
